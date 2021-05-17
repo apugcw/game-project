e7827f530dfbc6e28ee126229b396111ef181d07
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-05-17 15:42:33 +0900

    add lesson01 files

---
+++ Learning/2021/L01_02_type.c
@@ -0,0 +1,21 @@
+/* gcw プログラミング講習会 */
+
+#include <stdio.h>
+
+int main(){
+  int integer = 0; /* 整数値 */
+  double number = 0.22; /* 実数値 */
+  char character = 'a'; /* 文字 (1文字) */
+
+  /* 整数値として表示 */
+  printf("%d\n", integer);
+
+  /* 実数値として表示 */
+  printf("%f\n", number);
+
+  /* 文字として表示 */
+  printf("%c\n", character);
+
+  return 0;
+}
+
